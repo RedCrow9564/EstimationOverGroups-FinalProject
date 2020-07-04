- Fixed a bug in tri-spectrum calculation, which was caused because the modulo operator in C++ behaves differently than the modulo operator in Python.

UnitTests/test_tri_spectrum_estimation.py
@@ -26,14 +26,17 @@ class TestTriSpectrumAlgorithms(unittest.TestCase):
         output (up to numerical inaccuracies) for some random signal.
 
         """
-        rng = Generator(PCG64(1995))
+        rng = Generator(PCG64(596))
         signal_length: int = 10
         observations: int = 8
         signal: Matrix = np.fft.fft(rng.standard_normal((observations, signal_length)))
         tri_spectrum_naive: ThreeDMatrix = estimate_tri_spectrum_naive(signal)
         tri_spectrum_improved: ThreeDMatrix = estimate_tri_spectrum_v2(signal)
         # Validate both tri-spectra are equal
-        self.assertTrue(np.allclose(tri_spectrum_naive, tri_spectrum_improved))
+        self.assertTrue(np.allclose(np.abs(tri_spectrum_naive), np.abs(tri_spectrum_improved)),
+                        msg=f'{np.max(np.abs(tri_spectrum_improved - tri_spectrum_naive))}')
+        self.assertTrue(np.allclose(np.angle(tri_spectrum_naive), np.angle(tri_spectrum_improved)),
+                        msg=f'{np.max(np.angle(tri_spectrum_improved) - np.angle(tri_spectrum_naive))}')
 
 
 if __name__ == '__main__':

covariance_estimation.py
@@ -23,7 +23,7 @@ def estimate_covariance_up_to_phases(observations_fourier: Matrix, signal_length
     tri_spectrum: ThreeDMatrix = estimate_tri_spectrum_v2(observations_fourier)
 
     # Optimization step
-    # g = perform_optimization(tri_spectrum, power_spectrum, signal_length, data_type)
+    #g = perform_optimization(tri_spectrum, power_spectrum, signal_length, data_type)
     # TODO: Implement the optimization step for both real and complex data.
     g = np.asfortranarray(np.tile(np.eye(signal_length, dtype=data_type), (signal_length - 1, 1, 1)))
 
@@ -34,6 +34,7 @@ def estimate_covariance_up_to_phases(observations_fourier: Matrix, signal_length
     exact_cov_fourier_basis: Matrix = np.conj(fft(exact_covariance, axis=0, norm="ortho").T)
     exact_cov_fourier_basis: Matrix = np.conj(fft(exact_cov_fourier_basis, axis=0, norm="ortho").T)
     return exact_cov_fourier_basis
+    # return estimated_covariance
 
 
 def create_optimization_objective(tri_spectrum, power_spectrum, data_type) -> Callable: